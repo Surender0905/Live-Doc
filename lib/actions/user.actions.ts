3b1685c7673193f8c559868b99b83420d4f9435e
Surender Pratap Singh <rk31151@gmail.com>
2025-01-06 19:47:09 +0530

    final commit

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use server";
+
+import { clerkClient } from "@clerk/nextjs/server";
+import { parseStringify } from "../utils";
+import liveblocks from "../liveblocks";
+
+export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
+    console.log(userIds, "userIds------");
+    try {
+        const clerk = await clerkClient();
+        // console.log(clerk, "clerk");
+        const { data } = await clerk.users.getUserList({ userIds });
+        // console.log(data, "data");
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const users = data.map((user: any) => ({
+            id: user.id,
+            name: `${user.firstName} ${user.lastName}`,
+            email: user.emailAddresses[0].emailAddress,
+            avatar: user.imageUrl,
+        }));
+
+        const sortedUsers = userIds.map((email) =>
+            users.find((user: any) => user.email === email),
+        );
+
+        return parseStringify(sortedUsers);
+    } catch (error) {
+        console.log(`Error fetching users: ${error}`);
+    }
+};
+
+export const getDocumentUsers = async ({
+    roomId,
+    currentUser,
+    text,
+}: {
+    roomId: string;
+    currentUser: string;
+    text: string;
+}) => {
+    try {
+        const room = await liveblocks.getRoom(roomId);
+
+        const users = Object.keys(room.usersAccesses).filter(
+            (email) => email !== currentUser,
+        );
+
+        if (text.length) {
+            const lowerCaseText = text.toLowerCase();
+
+            const filteredUsers = users.filter((email: string) =>
+                email.toLowerCase().includes(lowerCaseText),
+            );
+
+            return parseStringify(filteredUsers);
+        }
+
+        return parseStringify(users);
+    } catch (error) {
+        console.log(`Error fetching document users: ${error}`);
+    }
+};
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://943f1902ff964db31bc7162da3608e76@o4508596911538176.ingest.us.sentry.io/4508596918484992",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
